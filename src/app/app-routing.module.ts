007f72357a7e1a5ade1cd3b346db6a771eebcce7
Matheus Vicente <mv_vieira96@hotmail.com>
2023-05-24 14:58:16 -0300

    Criação de Componentes e definição de Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {path:'', redirectTo:'portfolio', pathMatch:'full'},
+
+  {path:'portfolio', loadChildren: ()=> import('./portfolio-section/portfolio-section.module')
+      .then(module => module.PortfolioSectionModule)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { Router, RouterLink } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileCardComponent,
+    AboutMeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterLink
   ],
   providers: [],
   bootstrap: [AppComponent]
