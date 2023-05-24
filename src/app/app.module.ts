e39fe5848a5a771b54e0b343ae9581ad5a6e0c8e
Matheus Vicente <mv_vieira96@hotmail.com>
2023-05-24 16:26:31 -0300

    Adicionando footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ProfileCardComponent } from './profile-card/profile-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    ProfileCardComponent
+    ProfileCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
