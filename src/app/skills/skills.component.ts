119574509919c9383a906fbfd35ae9dc04484d39
Matheus Vicente <mv_vieira96@hotmail.com>
2023-06-02 16:53:20 -0300

    Adicionando icones a página de habilidades

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+
+export class SkillsComponent implements OnInit {
+  ngOnInit(): void {
+
+  }
 
 }
